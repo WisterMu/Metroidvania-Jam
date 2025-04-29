371e1fd4222390de45af29f34afa56664c318efa
WisterMu <Shawn.wu5813@gmail.com>
2025-04-28 22:42:35 -0400

    added comments to script

--- Metroidvania game/Assets/Player Moves/PlayerMovement.cs
+++ Metroidvania game/Assets/Player Moves/PlayerMovement.cs
@@ -18,15 +18,18 @@ public class PlayerMovement : MonoBehaviour
 
     public void Move(InputAction.CallbackContext context)
     {
+        //movement left and right
         horizontalMovement = context.ReadValue<Vector2>().x;
     }
 
     public void Jump(InputAction.CallbackContext context)
     {
+        //Jump when holding space
         if (context.performed)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpPower);
         }
+        //Short jump when tapping space
         else if (context.canceled)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);

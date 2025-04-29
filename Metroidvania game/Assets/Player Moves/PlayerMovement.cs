217f523384d24ec1c2070a5caf6b97d5c14f847d
WisterMu <Shawn.wu5813@gmail.com>
2025-04-28 22:36:51 -0400

    Added jumping and camera follows player

--- Metroidvania game/Assets/Player Moves/PlayerMovement.cs
+++ Metroidvania game/Assets/Player Moves/PlayerMovement.cs
@@ -9,6 +9,8 @@ public class PlayerMovement : MonoBehaviour
 
     float horizontalMovement;
 
+    public float jumpPower = 10f;
+
     void Update()
     {
         rb.linearVelocity = new Vector2(horizontalMovement * moveSpeed, rb.linearVelocity.y);
@@ -17,6 +19,18 @@ public class PlayerMovement : MonoBehaviour
     public void Move(InputAction.CallbackContext context)
     {
         horizontalMovement = context.ReadValue<Vector2>().x;
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpPower);
+        }
+        else if (context.canceled)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
+        }
     }
 
 }
